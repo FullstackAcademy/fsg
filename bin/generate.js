709a110f475bead6a783319f8488313c6d416086
Joseph Alves <databentpx@gmail.com>
2015-02-25 22:29:00 -0500

    Updating

---
+++ bin/generate.js
@@ -0,0 +1,18 @@
+var path = require('path');
+var chalk = require('chalk');
+var ncp = require('ncp').ncp;
+
+ncp.limit = 16;
+
+var newProjectDir = process.cwd();
+var generatorFilesPath = path.join(__dirname, '../generated');
+
+console.log(chalk.blue('Generating your poppin\' fresh new application!'));
+
+ncp(generatorFilesPath, newProjectDir, function (err) {
+    if (err) return console.error(err);
+    console.log(chalk.blue('All done!'));
+    console.log(chalk.red('Don\'t forget to npm install!'));
+    console.log(chalk.red('Don\'t forget to bower install!'));
+    console.log(chalk.yellow('Try out npm start!'));
+});
