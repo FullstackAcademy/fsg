9feb808904bdcf874bb06d24bf6ecd3901c5ee82
Joseph Alves <databentpx@gmail.com>
2015-03-02 14:49:08 -0500

    INSERT placeholders for third party auth

--- generated/server/env/development.js
+++ generated/server/env/development.js
@@ -2,18 +2,18 @@ module.exports = {
   "DATABASE_URI": "mongodb://localhost:27017/my-app",
   "SESSION_SECRET": "Optimus Prime is my real dad",
   "TWITTER": {
-    "consumerKey": "ZVQ2NZscYX1aa6nZI81Fg8HBg",
-    "consumerSecret": "FkNrAsPhXVwr1qKgV5fSMGtuTsQNoWqybHlkKFf4g6FG1n4J1V",
-    "callbackUrl": "http://127.0.0.1:1337/auth/twitter/callback"
+    "consumerKey": "INSERT_TWITTER_CONSUMER_KEY_HERE",
+    "consumerSecret": "INSERT_TWITTER_CONSUMER_SECRET_HERE",
+    "callbackUrl": "INSERT_TWITTER_CALLBACK_HERE"
   },
   "FACEBOOK": {
-    "clientID": "336560806541655",
-    "clientSecret": "2f117d2a289fd321e411fdb568d263f2",
-    "callbackURL": "http://localhost:1337/auth/facebook/callback"
+    "clientID": "INSERT_FACEBOOK_CLIENTID_HERE",
+    "clientSecret": "INSERT_FACEBOOK_CLIENT_SECRET_HERE",
+    "callbackURL": "INSERT_FACEBOOK_CALLBACK_HERE"
   },
   "GOOGLE": {
-    "clientID": "369003002521-88250fc9kjpkfqvhicu7vokbnvu88ud6.apps.googleusercontent.com",
-    "clientSecret": "DB1-q81E4ODEuL8cFGCm5QS1",
-    "callbackURL": "http://localhost:1337/auth/google/callback"
+    "clientID": "INSERT_GOOGLE_CLIENTID_HERE",
+    "clientSecret": "INSERT_GOOGLE_CLIENT_SECRET_HERE",
+    "callbackURL": "INSERT_GOOGLE_CALLBACK_HERE"
   }
 };
