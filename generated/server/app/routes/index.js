9c6b400f10be541578fdee3557b0cfd75b33a11a
Joseph Alves <databentpx@gmail.com>
2015-03-04 10:43:54 -0500

    404 for /api calls

--- generated/server/app/routes/index.js
+++ generated/server/app/routes/index.js
@@ -3,3 +3,9 @@ var router = require('express').Router();
 module.exports = router;
 
 router.use('/tutorial', require('./tutorial'));
+
+// Make sure this is after all of
+// the registered routes!
+router.use(function (req, res) {
+    res.status(404).end();
+});
