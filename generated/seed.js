82686361d7ee58d9d64215932d9fa49ad7aad9da
Joseph Alves <databentpx@gmail.com>
2015-04-16 00:32:37 -0400

    Added seed file to root of generated application; changed the development database name from my-app to fsg-app

---
+++ generated/seed.js
@@ -0,0 +1,64 @@
+/*
+
+This seed file is only a placeholder. It should be expanded and altered
+to fit the development of your application.
+
+It uses the same file the server uses to establish
+the database connection:
+--- server/db/index.js
+
+The name of the database used is set in your environment files:
+--- server/env/*
+
+This seed file has a safety check to see if you already have users
+in the database. If you are developing multiple applications with the
+fsg scaffolding, keep in mind that fsg always uses the same database
+name in the environment files.
+
+Refer to the q documentation for why and how q.invoke is used.
+
+*/
+
+var mongoose = require('mongoose');
+var connectToDb = require('./server/db');
+var User = mongoose.model('User');
+var q = require('q');
+var chalk = require('chalk');
+
+var getCurrentUserData = function () {
+    return q.ninvoke(User, 'find', {});
+};
+
+var seedUsers = function () {
+
+    var users = [
+        {
+            email: 'testing@fsa.com',
+            password: 'password'
+        },
+        {
+            email: 'obama@gmail.com',
+            password: 'potus'
+        }
+    ];
+
+    return q.invoke(User, 'create', users);
+
+};
+
+connectToDb.then(function () {
+    getCurrentUserData().then(function (users) {
+        if (users.length === 0) {
+            return seedUsers();
+        } else {
+            console.log(chalk.magenta('Seems to already be user data, exiting!'));
+            process.kill(0);
+        }
+    }).then(function () {
+        console.log(chalk.green('Seed successful!'));
+        process.kill(0);
+    }).catch(function (err) {
+        console.error(err);
+        process.kill(1);
+    });
+});
--- generated/server/env/development.js
+++ generated/server/env/development.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "DATABASE_URI": "mongodb://localhost:27017/my-app",
+  "DATABASE_URI": "mongodb://localhost:27017/fsg-app",
   "SESSION_SECRET": "Optimus Prime is my real dad",
   "TWITTER": {
     "consumerKey": "INSERT_TWITTER_CONSUMER_KEY_HERE",
