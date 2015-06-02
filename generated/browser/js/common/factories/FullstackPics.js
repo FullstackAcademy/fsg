7b545f494599480f05846d5c44eea8161d95dc66
Joseph Alves <databentpx@gmail.com>
2015-06-02 12:00:13 -0400

    About page tweaks.

---
+++ generated/browser/js/common/factories/FullstackPics.js
@@ -0,0 +1,25 @@
+app.factory('FullstackPics', function () {
+    return [
+        'https://pbs.twimg.com/media/B7gBXulCAAAXQcE.jpg:large',
+        'https://fbcdn-sphotos-c-a.akamaihd.net/hphotos-ak-xap1/t31.0-8/10862451_10205622990359241_8027168843312841137_o.jpg',
+        'https://pbs.twimg.com/media/B-LKUshIgAEy9SK.jpg',
+        'https://pbs.twimg.com/media/B79-X7oCMAAkw7y.jpg',
+        'https://pbs.twimg.com/media/B-Uj9COIIAIFAh0.jpg:large',
+        'https://pbs.twimg.com/media/B6yIyFiCEAAql12.jpg:large',
+        'https://pbs.twimg.com/media/CE-T75lWAAAmqqJ.jpg:large',
+        'https://pbs.twimg.com/media/CEvZAg-VAAAk932.jpg:large',
+        'https://pbs.twimg.com/media/CEgNMeOXIAIfDhK.jpg:large',
+        'https://pbs.twimg.com/media/CEQyIDNWgAAu60B.jpg:large',
+        'https://pbs.twimg.com/media/CCF3T5QW8AE2lGJ.jpg:large',
+        'https://pbs.twimg.com/media/CAeVw5SWoAAALsj.jpg:large',
+        'https://pbs.twimg.com/media/CAaJIP7UkAAlIGs.jpg:large',
+        'https://pbs.twimg.com/media/CAQOw9lWEAAY9Fl.jpg:large',
+        'https://pbs.twimg.com/media/B-OQbVrCMAANwIM.jpg:large',
+        'https://pbs.twimg.com/media/B9b_erwCYAAwRcJ.png:large',
+        'https://pbs.twimg.com/media/B5PTdvnCcAEAl4x.jpg:large',
+        'https://pbs.twimg.com/media/B4qwC0iCYAAlPGh.jpg:large',
+        'https://pbs.twimg.com/media/B2b33vRIUAA9o1D.jpg:large',
+        'https://pbs.twimg.com/media/BwpIwr1IUAAvO2_.jpg:large',
+        'https://pbs.twimg.com/media/BsSseANCYAEOhLw.jpg:large'
+    ];
+});
