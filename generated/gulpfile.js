ee0ed241655bc802b7f3ff21752f5207571e92a9
Joe Alves <joe@fullstackacademy.com>
2015-07-31 16:48:56 -0400

    Changes to static middleware for uiBootstrap; server tests now es6-enabled

--- generated/gulpfile.js
+++ generated/gulpfile.js
@@ -50,6 +50,7 @@ gulp.task('buildJS', ['lintJS'], function () {
 });
 
 gulp.task('testServerJS', function () {
+    require('babel/register');
 	return gulp.src('./tests/server/**/*.js', {
 		read: false
 	}).pipe(mocha({ reporter: 'spec' }));
