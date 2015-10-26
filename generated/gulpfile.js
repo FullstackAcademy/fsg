73909ee450c398f8741877b65acbd0a3857f3a56
Joe Alves <joe@fullstackacademy.com>
2015-10-26 10:45:04 -0400

    Removed seed task from gulpfile

--- generated/gulpfile.js
+++ generated/gulpfile.js
@@ -96,27 +96,6 @@ gulp.task('buildCSS', function () {
         .pipe(gulp.dest('./public'));
 });
 
-gulp.task('seedDB', function () {
-
-    var users = [
-        { email: 'testing@fsa.com', password: 'testing123' },
-        { email: 'joe@fsa.com', password: 'rainbowkicks' },
-        { email: 'obama@gmail.com', password: 'potus' }
-    ];
-
-    var dbConnected = require('./server/db');
-
-    return dbConnected.then(function () {
-        var User = require('mongoose').model('User');
-        return User.create(users);
-    }).then(function () {
-        process.kill(0);
-    }).catch(function (err) {
-        console.error(err);
-    });
-
-});
-
 // Production tasks
 // --------------------------------------------------------------
 
