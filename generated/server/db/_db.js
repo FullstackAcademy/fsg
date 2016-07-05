e8ea0a6e4edd989c8730bb3fc17c57edc3724c26
Gabriel Lebec <glebec@gmail.com>
2016-07-05 05:17:50 -0400

    enable pg-native for faster database

--- generated/server/db/_db.js
+++ generated/server/db/_db.js
@@ -2,6 +2,9 @@ var path = require('path');
 var Sequelize = require('sequelize');
 
 var env = require(path.join(__dirname, '../env'));
-var db = new Sequelize(env.DATABASE_URI, { logging: env.LOGGING });
+var db = new Sequelize(env.DATABASE_URI, {
+  logging: env.LOGGING,
+  native: env.NATIVE
+});
 
 module.exports = db;
--- generated/server/env/development.js
+++ generated/server/env/development.js
@@ -16,5 +16,6 @@ module.exports = {
     "clientSecret": "INSERT_GOOGLE_CLIENT_SECRET_HERE",
     "callbackURL": "INSERT_GOOGLE_CALLBACK_HERE"
   },
-  "LOGGING": true
+  "LOGGING": true,
+  "NATIVE": true
 };
