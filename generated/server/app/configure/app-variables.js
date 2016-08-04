8a02b91ff7dd0c1120b2a4c390c53640741bc436
Danielle Gellis <danisyellis@gmail.com>
2016-08-04 15:50:59 -0400

    move faviconPath before indexPath so that the browser downloads the new favicon before loading pages- otherwise the browser does not update the favicon

--- generated/server/app/configure/app-variables.js
+++ generated/server/app/configure/app-variables.js
@@ -4,8 +4,8 @@ var chalk = require('chalk');
 var util = require('util');
 
 var rootPath = path.join(__dirname, '../../../');
-var indexPath = path.join(rootPath, './server/app/views/index.html');
 var faviconPath = path.join(rootPath, './server/app/views/favicon.ico');
+var indexPath = path.join(rootPath, './server/app/views/index.html');
 
 var env = require(path.join(rootPath, './server/env'));
 
