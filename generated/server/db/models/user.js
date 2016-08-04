546c7dcd5a6a301b40a82720966786e05d376b1d
Jason Unger <jasonscottunger@gmail.com>
2016-08-04 12:16:48 -0400

    Updated user hooks for updating password

--- generated/server/db/models/user.js
+++ generated/server/db/models/user.js
@@ -45,7 +45,11 @@ module.exports = db.define('user', {
         }
     },
     hooks: {
-        beforeValidate: function (user) {
+        beforeCreate: function (user) {
+            user.salt = user.Model.generateSalt();
+            user.password = user.Model.encryptPassword(user.password, user.salt);
+        },
+        beforeUpdate: function (user) {
             if (user.changed('password')) {
                 user.salt = user.Model.generateSalt();
                 user.password = user.Model.encryptPassword(user.password, user.salt);
