11a17f9d6708c7d1c69a2a0f9d22fdfbbad0683e
Joe Alves <joe@fullstackacademy.com>
2016-09-14 19:04:59 -0400

    Changed how db connection was using the logging env variable to prevent deprecation warning

--- generated/server/db/_db.js
+++ generated/server/db/_db.js
@@ -3,7 +3,7 @@ var Sequelize = require('sequelize');
 
 var env = require(path.join(__dirname, '../env'));
 var db = new Sequelize(env.DATABASE_URI, {
-  logging: env.LOGGING,
+  logging: env.LOGGING ? console.log : false,
   native: env.NATIVE
 });
 
