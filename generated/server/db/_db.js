e3c158cea31c2864281959a0d2db3a13150ec6f2
Joe Alves <joe@fullstackacademy.com>
2016-09-16 13:45:32 -0400

    Fixing database url environment variable to heroku new default

--- generated/server/db/_db.js
+++ generated/server/db/_db.js
@@ -2,7 +2,7 @@ var path = require('path');
 var Sequelize = require('sequelize');
 
 var env = require(path.join(__dirname, '../env'));
-var db = new Sequelize(env.DATABASE_URI, {
+var db = new Sequelize(env.DATABASE_URL, {
   logging: env.LOGGING ? console.log : false,
   native: env.NATIVE
 });
--- generated/server/env/production.js
+++ generated/server/env/production.js
@@ -7,7 +7,7 @@
  */
 
 module.exports = {
-    DATABASE_URI: process.env.DATABASE_URI,
+    DATABASE_URL: process.env.DATABASE_URL,
     SESSION_SECRET: process.env.SESSION_SECRET,
     TWITTER: {
         consumerKey: process.env.TWITTER_CONSUMER_KEY,
