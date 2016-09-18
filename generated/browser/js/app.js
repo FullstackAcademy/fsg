d8d9d9bb873d1e8bdc7822333f6102f8a599df60
Gabriel Lebec <glebec@gmail.com>
2016-09-18 17:33:01 -0400

    provide toState name in error message

--- generated/browser/js/app.js
+++ generated/browser/js/app.js
@@ -15,7 +15,7 @@ app.config(function ($urlRouterProvider, $locationProvider) {
 // This app.run is for listening to errors broadcasted by ui-router, usually originating from resolves
 app.run(function ($rootScope) {
     $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, thrownError) {
-        console.info('The following error was thrown by ui-router. The origin of this error is probably a resolve function:');
+        console.info(`The following error was thrown by ui-router while transitioning to state "${toState.name}". The origin of this error is probably a resolve function:`);
         console.error(thrownError);
     });
 });
