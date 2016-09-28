0360777b58dc1cb9856183ba41c0081f492078fa
Jason Hang <dev.jdhang@gmail.com>
2016-09-28 18:26:13 -0400

    Fixed testing so webpack doesn't build during it. Added some more specific npm scripts.

--- generated/server/app/configure/webpack-middleware.js
+++ generated/server/app/configure/webpack-middleware.js
@@ -1,9 +1,10 @@
 'use strict';
 
 const path = require('path');
+const NODE_ENV = process.env.NODE_ENV;
 
 module.exports = function (app) {
-  if (process.env.NODE_ENV !== 'production') {
+  if (NODE_ENV !== 'production' && NODE_ENV !== 'testing') {
     const webpack = require('webpack');
     const webpackHotMiddleware = require('webpack-hot-middleware');
     const webpackDevMiddleware = require('webpack-dev-middleware');
